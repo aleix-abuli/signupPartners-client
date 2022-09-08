dc36b0c5db12b02b8a008d06509da259d67d6ccf
aleix-abuli <aleixabuli088@gmail.com>
2022-09-08 11:51:48 +0200

    media queries for landing page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,8 @@ import './Footer.css';
 export default function Footer() {
     return(
         <footer>
-            <p className='footerText' >Glovo Partners</p>
+            <p className='footerText'>Glovo Partners.</p>
+            <p className='footerText tm'>Mock up coded by Aleix Abulí.</p>
         </footer>
     );
 };
